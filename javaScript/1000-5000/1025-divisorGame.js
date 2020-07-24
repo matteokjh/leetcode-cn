0730f6fadf1de41ba2cf6e92b4d5b5cd62da4363
matteokjh <429797371@qq.com>
2020-07-24 23:33:30 +0800

    1025

---
+++ javaScript/1000-5000/1025-divisorGame.js
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode-cn.com/problems/divisor-game/
+ * 1025. 除数博弈
+ * Easy
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame = function(N) {
+    return N % 2 === 0
+};
